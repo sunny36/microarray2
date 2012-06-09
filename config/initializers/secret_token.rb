f3ee6584b5c1eef672016b4da1c22328e5b1de34
Somchok Sakjiraphong <s@sunny.in.th>
2012-06-09 20:34:43 +0700

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Microarray2::Application.config.secret_token = '257fe64f4a0a91aed7efe4359e84317a63fba336111ceb9fd9d3c84e56b11b6baf0e2ad31bbe83c59c0e6abc08d1f1fc27fdc2d3a5d22230867a78b64fabc0de'
